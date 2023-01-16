79f8fa108ae1360ce6d9bb7d5502d40a74359934
vinodkumar2704 <vinodkb2704@gmail.com>
2023-01-16 22:37:58 +0530

    added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,14 @@ export default function About() {
     return(
         <div>
         <Navbar></Navbar>
-        hello world</div>
+        
+
+      <div className="blog">
+      <h1>Fresh Bakery</h1>
+      <p>A bakery is an establishment that produces and sells flour-based food baked in an oven such as bread, cookies, cakes, donuts, pastries, and pies. Some retail bakeries are also categorized as cafés, serving coffee and tea to customers who wish to consume the baked goods on the premises.</p>
+      <p> With our bakery we provide hot baked products broought to you fresh.</p>
+      </div>
+      </div>
     )
     
   }
